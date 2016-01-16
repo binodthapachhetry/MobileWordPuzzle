30c75cca1cbfec00ee09a9ed465f42f72dc2a502
binod thapa chhetry <jarvis@binods-MacBook-Air.local>
2016-01-16 11:36:52 -0500

    created the main UI/activity following Burnette's book

--- app/src/main/java/edu/neu/madcourse/binodthapachhetry/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/binodthapachhetry/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/edu/neu/madcourse/binodthapachhetry/MainActivityFragment.java
+++ app/src/main/java/edu/neu/madcourse/binodthapachhetry/MainActivityFragment.java
@@ -1,13 +1,17 @@
 package edu.neu.madcourse.binodthapachhetry;
 
-import android.support.v4.app.Fragment;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+//import android.support.v4.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
+import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,7 +20,9 @@ import java.util.Arrays;
  */
 public class MainActivityFragment extends Fragment {
 
-    private ArrayAdapter<String> mItemAdapter;
+//    private ArrayAdapter<String> mItemAdapter;
+
+    private AlertDialog mDialog;
 
     public MainActivityFragment() {
     }
@@ -26,21 +32,54 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // List of items in the main menu
-        String[] itemArray = {"About","GenerateError","TicTacToe","Quit"};
-        ArrayList<String> allItems = new ArrayList<String>(Arrays.asList(itemArray));
+        // Handle buttons here
+        View aboutButton = rootView.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_title);
+                builder.setMessage(R.string.about_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface,
+                                                int i) {
+                                // nothing
+                            }
+                        });
+                mDialog = builder.show();
+            }
+        });
 
-        // Creating an ArrayAdapter
-        mItemAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(),
-                        R.layout.list_item,
-                        R.id.list_item_textview,
-                        allItems);
-        // Get a reference to the ListView, and attach this adapter
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_item);
-        listView.setAdapter(mItemAdapter);
+
+
+        // List of items in the main menu
+//        String[] itemArray = {"About","GenerateError","TicTacToe","Quit"};
+//        ArrayList<String> allItems = new ArrayList<String>(Arrays.asList(itemArray));
+//
+//        // Creating an ArrayAdapter
+//        mItemAdapter =
+//                new ArrayAdapter<String>(
+//                        getActivity(),
+//                        R.layout.list_item,
+//                        R.id.list_item_textview,
+//                        allItems);
+//        // Get a reference to the ListView, and attach this adapter
+//        ListView listView = (ListView) rootView.findViewById(R.id.listview_item);
+//        listView.setAdapter(mItemAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Get rid of the about dialog if it's still up
+        if (mDialog != null)
+            mDialog.dismiss();
     }
 }
